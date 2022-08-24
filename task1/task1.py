bc81ebd53493de89bbfbdad5daed03786ea719df
Anvar <ankhafizov1998@yandex.ru>
2022-08-24 18:28:24 +0300

    dd more optimized solution

--- task1/task1.py
+++ task1/task1.py
@@ -1,15 +1,16 @@
 from functools import reduce
 
 def process_func(arr):
-    new_arr = []
-    if len(arr) == 1:
-        new_arr = arr
-    else:
-        for i, _ in enumerate(arr):
-            new_arr.append(reduce(lambda x, y: x*y, arr[:i]+arr[i+1:]))
+    new_arr=list(
+        map(lambda indx: reduce(lambda x, y: x*y, arr[:indx]+arr[indx+1:]) if len(arr)>1 else arr[0], range(len(arr)))
+    )
 
     return new_arr
 
 if __name__ == "__main__":
     input_array = [1, 2, 3]
+
+    assert len(input_array) > 0, "Согласно заданию, список долен быть ненулевой длины"
+    assert set([type(elem) for elem in input_array])=={int}, "Согласно заданию, список должен быть только из целочисленных элементов"
+
     print(process_func(input_array))
