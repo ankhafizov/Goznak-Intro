e65c819f0dd591467b18c682446736e3f9a61876
Anvar Khafizov <akhafizov@croc.ru>
2022-09-04 16:30:57 +0300

    code refactored

---
+++ task2/inference_classify.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import numpy as np
+from utils.utils import load_mel
+
+
+def predict_single_mel_npy_file(npy_path, model, classes=("clean", "noisy")):
+    """ Сделать инференс на 1 файле """
+    input_shape = model.layers[0].input_shape[1:]
+
+    image_arr = np.expand_dims(load_mel(npy_path), axis=-1)
+    image_arr = np.dstack([image_arr for _ in range(input_shape[-1])])
+    image_arr = tf.image.resize(image_arr, input_shape[:-1]).numpy()
+    image_arr = tf.keras.utils.normalize(image_arr)
+    class_indx = np.argmax(model.predict(image_arr[tf.newaxis]), axis=1)[0]
+    return classes[class_indx]
+
+
+if __name__ == "__main__":
+    # ============= Эту часть можно менять ===============================
+    filepaths = ["data/val/clean/264/264_121332_264-121332-0017.npy",
+                 "data/val/clean/614/614_6500_614-6500-0012.npy",
+                 "data/val/noisy/273/273_123248_273-123248-0014.npy",
+                 "data/val/noisy/1154/1154_129975_1154-129975-0005.npy"]
+    # ====================================================================
+
+    trained_model_filepath = "pretrained_models/classification_clean_noisy"
+    model = tf.keras.models.load_model(trained_model_filepath)
+
+    print("\n output:")
+    for pth in filepaths:
+        print(pth, ":", predict_single_mel_npy_file(pth, model))
